28df76249b5054879246ef7c80bcbc0a597e9e2b
breeze98 <breezeyyh@gmail.com>
2022-09-21 16:36:09 +0800

    doc

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -8,8 +8,8 @@ type ChainTokenList = {
 const tokens: ChainTokenList = {
   [ChainId.MAINNET]: [
     new Token(ChainId.MAINNET, '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7', 18, 'ETH', 'Ether'),
-    // new Token(ChainId.MAINNET, '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3', 18, 'DAI', 'Dai Stablecoin'),
     new Token(ChainId.MAINNET, '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8', 6, 'USDC', 'USD Coin'),
+    // new Token(ChainId.MAINNET, '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3', 18, 'DAI', 'Dai Stablecoin'),
     // new Token(ChainId.MAINNET, '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac', 8, 'WBTC', 'Wrapped BTC'),
     // new Token(ChainId.MAINNET, '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8', 6, 'USDT', 'Tether USD'),
   ],
