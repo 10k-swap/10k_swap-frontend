e029f9d53cf220044d6b55ba477e1b8efd61af64
breeze98 <breezeyyh@gmail.com>
2022-09-22 11:31:27 +0800

    refactor:add return type

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -2,9 +2,6 @@ import { number } from 'starknet'
 import * as buffer from 'buffer'
 import { BN } from '../types'
 
-export function BN2String(value: BN | undefined) {
-  if (!value) {
-    return ''
-  }
-  return number.toBN(value).toArrayLike(buffer.Buffer, 'utf-8').toString()
+export function parseBN2String(value: BN | undefined): string {
+  return value ? number.toBN(value).toArrayLike(buffer.Buffer, 'utf-8').toString() : ''
 }
