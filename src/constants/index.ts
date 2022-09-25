d43a90ee7398442ebfb0ac4778ce093a8bf6f863
breeze98 <breezeyyh@gmail.com>
2022-09-25 15:41:42 +0800

    update allowed slippage

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,7 +8,7 @@ export const ADDRESS_ZORE = '0x0000000000000000000000000000000000000000000000000
 export const MOBILE_SIZE = 768
 
 // default allowed slippage, in bips
-export const INITIAL_ALLOWED_SLIPPAGE = 20
+export const INITIAL_ALLOWED_SLIPPAGE = 200
 
 // 60 minutes, denominated in seconds
 export const DEFAULT_DEADLINE_FROM_NOW = 60 * 60
