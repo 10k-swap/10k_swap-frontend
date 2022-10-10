81fb210b099e10fe11b26c320fdf5a8dc58297d0
breeze98 <breezeyyh@gmail.com>
2022-10-10 11:18:38 +0800

    update get pool

--- src/state/pool/hooks.ts
+++ src/state/pool/hooks.ts
@@ -1,8 +1,25 @@
-import { computed } from 'vue'
+import { computed, onMounted, watch } from 'vue'
 import { usePoolStore } from '.'
+import { useStarknet } from '../../starknet-vue/providers/starknet'
 
 export function useAllPairs() {
   const store = usePoolStore()
+
+  const {
+    state: { chainId },
+  } = useStarknet()
+
+  onMounted(() => {
+    if (chainId.value) {
+      store.getAllPairs(chainId.value)
+    }
+  })
+  watch([chainId], () => {
+    if (chainId.value) {
+      store.getAllPairs(chainId.value)
+    }
+  })
+
   return computed(() => store.$state.pairs)
 }
 
@@ -13,5 +30,21 @@ export function useIsLoadingAllPairs() {
 
 export function useUserPairs() {
   const store = usePoolStore()
-  return computed(() => store.$state.userPools)
+  const pairs = useAllPairs()
+  const userPools = computed(() => store.$state.userPools)
+
+  const {
+    state: { chainId, account },
+  } = useStarknet()
+
+  const _getUserPairs = () => {
+    if (pairs.value.length && account.value && chainId.value && !userPools.value.length) {
+      store.getUserPairs(chainId.value, account.value)
+    }
+  }
+
+  watch([pairs, account, chainId], _getUserPairs)
+  onMounted(_getUserPairs)
+
+  return userPools
 }
