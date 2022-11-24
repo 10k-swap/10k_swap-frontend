be1243b2af603412b89ab68d1930f4ab1a1894be
breeze98 <breezeyyh@gmail.com>
2022-11-24 11:03:07 +0800

    fix call bug

--- src/starknet-vue/hooks/call.ts
+++ src/starknet-vue/hooks/call.ts
@@ -69,7 +69,7 @@ export function useStarknetCall<T extends unknown[]>(
       }
 
       if (current && isAvailableCache(current.updateAt)) {
-        return current.result
+        return current
       }
 
       try {
