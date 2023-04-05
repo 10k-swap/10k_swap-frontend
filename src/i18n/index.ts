db47a59c784521962aaa9a2477c8695151d756bc
0x60018 <0x60018@gmail.com>
2023-04-05 22:18:26 +0800

    chore: add zh-TW and he-IL

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,19 +1,23 @@
 import { createI18n } from 'vue-i18n'
 import enUS from './locales/en-US'
 import zhCN from './locales/zh-CN'
+import zhTW from './locales/zh-TW'
 import frFR from './locales/fr-FR'
 import jaJP from './locales/ja-JP'
 import koKR from './locales/ko-KR'
+import heIL from './locales/he-IL'
 
 const i18n = createI18n({
   legacy: false,
-  locale: 'ko-KR',
+  locale: 'he-IL',
   messages: {
     'en-US': enUS,
     'zh-CN': zhCN,
+    'zh-TW': zhTW,
     'fr-FR': frFR,
     'ja-JP': jaJP,
     'ko-KR': koKR,
+    'he-IL': heIL,
   },
 })
 
