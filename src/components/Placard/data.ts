aa0ea3ba8e16a37eaf84a24764a2f9d6374f42b5
breeze98 <breezeyyh@gmail.com>
2024-01-23 14:46:53 +0800

    add tips

--- src/components/Placard/data.ts
+++ src/components/Placard/data.ts
@@ -1,9 +1,14 @@
-import i18n from '../../i18n'
+// import i18n from '../../i18n'
 import { Placard } from './types'
 
 export const placards: Placard[] = [
+  // {
+  //   content: i18n.global.t('warning'),
+  //   type: 'warning',
+  // },
   {
-    content: i18n.global.t('warning'),
+    content:
+      'StarkWare will deploy a new DAI contract on Starknet. Please withdraw the liquidity and re-establish the DAI bridge. For specific details, please check:<a herf="https://x.com/MakerDAO/status/1746977683190251591?s=20">https://x.com/MakerDAO/status/1746977683190251591?s=20<a/> ',
     type: 'warning',
   },
 ]
