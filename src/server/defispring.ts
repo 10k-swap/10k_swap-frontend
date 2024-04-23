ad19b71b34b5aa03f8b5e84a44e390082eacdcf1
0x60018 <0x60018@gmail.com>
2024-04-24 00:22:08 +0800

    chore: updates

--- src/server/defispring.ts
+++ src/server/defispring.ts
@@ -1,17 +1,18 @@
 import { StarknetChainId } from 'l0k_swap-sdk'
 import { SERVER_URLS } from '../constants'
 import axios from './axios'
+import { sleep } from '../utils'
 
 export interface DefispringCalldata {
   amount: string
   proof: string[]
 }
 
-export async function getCalldata(chainId: StarknetChainId, account?: string, round?: number) {
-  try {
-    if (chainId !== StarknetChainId.MAINNET) throw new Error('Not support testnet')
-    if (!account) throw new Error('Miss account')
+export async function getCalldata(chainId: StarknetChainId, account?: string, round?: number, retryNumber = 0) {
+  if (chainId !== StarknetChainId.MAINNET) throw new Error('Not support testnet')
+  if (!account) throw new Error('Miss account')
 
+  try {
     const res = await axios.get<DefispringCalldata>(`${SERVER_URLS[chainId]}/defispring/get_calldata`, { params: { address: account, round } })
 
     if (res.data) {
@@ -20,6 +21,10 @@ export async function getCalldata(chainId: StarknetChainId, account?: string, ro
 
     throw new Error('fetch pairs fail')
   } catch (error: any) {
-    return { amount: '0x00', proof: [] } as DefispringCalldata
+    retryNumber += 1
+    await sleep(Math.min(retryNumber * 200, 5000))
+    return await getCalldata(chainId, account, round, retryNumber)
+
+    // return { amount: '0x00', proof: [] } as DefispringCalldata
   }
 }
