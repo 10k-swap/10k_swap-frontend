61fc3572547a4f688de449797ff5d0a833ff379f
0x60018 <0x60018@gmail.com>
2024-05-06 19:05:56 +0800

    chore: updates

--- src/server/defispring.ts
+++ src/server/defispring.ts
@@ -1,5 +1,4 @@
 import { StarknetChainId } from 'l0k_swap-sdk'
-import { SERVER_URLS } from '../constants'
 import axios from './axios'
 import { sleep } from '../utils'
 
@@ -8,12 +7,12 @@ export interface DefispringCalldata {
   proof: string[]
 }
 
-export async function getCalldata(chainId: StarknetChainId, account?: string, round?: number, retryNumber = 0) {
+export async function getCalldata(chainId: StarknetChainId, account?: string, round?: number, retryNumber = 0): Promise<DefispringCalldata> {
   if (chainId !== StarknetChainId.MAINNET) throw new Error('Not support testnet')
   if (!account) throw new Error('Miss account')
 
   try {
-    const res = await axios.get<DefispringCalldata>(`https://10kswap.defispring.xyz/defispring/get_calldata`, { params: { address: account, round } })
+    const res = await axios.get<DefispringCalldata>('https://10kswap.defispring.xyz/defispring/get_calldata', { params: { address: account, round } })
 
     if (res.data) {
       return res.data
