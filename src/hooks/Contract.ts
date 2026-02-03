00642bac2d77e4008c15b38539421420c9d8634e
0x60018 <0x60018@gmail.com>
2026-02-03 22:08:18 +0800

    fixed: rpc bugs

--- src/hooks/Contract.ts
+++ src/hooks/Contract.ts
@@ -3,11 +3,11 @@ import { computed, ComputedRef } from 'vue'
 import erc20 from '../constants/abis/erc20.json'
 import naming_abi from '../constants/abis/naming_abi.json'
 import { STARKNET_ID_ADDRESSES } from '../constants/address'
-import { useContract } from '../starknet-vue/hooks/contract'
+import { useContract, useContractDefault } from '../starknet-vue/hooks/contract'
 import { useStarknet } from '../starknet-vue/providers/starknet'
 
 export function useTokenContract(address: ComputedRef<string | undefined>) {
-  return useContract(
+  return useContractDefault(
     computed(() => erc20 as Abi),
     address
   )
--- src/starknet-vue/hooks/contract.ts
+++ src/starknet-vue/hooks/contract.ts
@@ -1,6 +1,7 @@
 import { Abi, Contract } from 'starknet5'
 import { computed, ComputedRef, toRaw } from 'vue'
 import { useStarknet } from '../providers/starknet'
+import { defaultProvider } from '../providers/starknet/const'
 
 export function useContract(abi: ComputedRef<Abi>, address: ComputedRef<string | undefined>): ComputedRef<Contract | undefined> {
   const {
@@ -10,6 +11,15 @@ export function useContract(abi: ComputedRef<Abi>, address: ComputedRef<string |
   return computed(() => {
     if (address.value && library.value) {
       return new Contract(abi.value, address.value, toRaw(library.value))
+    }
+    return undefined
+  })
+}
+
+export function useContractDefault(abi: ComputedRef<Abi>, address: ComputedRef<string | undefined>): ComputedRef<Contract | undefined> {
+  return computed(() => {
+    if (address.value && defaultProvider) {
+      return new Contract(abi.value, address.value, defaultProvider)
     }
     return undefined
   })
